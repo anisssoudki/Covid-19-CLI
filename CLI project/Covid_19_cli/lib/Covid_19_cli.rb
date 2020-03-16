5b92a91d99bba8b981f90a8af2a2f8cf1e6195c7
Aniss <59289660+anisssoudki@users.noreply.github.com>
2020-03-16 11:11:18 -0700

    Add files via upload

---
+++ CLI project/Covid_19_cli/lib/Covid_19_cli.rb
@@ -0,0 +1,8 @@
+require_relative "./Covid_19_cli/version"
+
+require_relative "./Covid_19_cli/cli.rb"
+require_relative "./Covid_19_cli/api.rb"
+require_relative "./Covid_19_cli/country.rb"
+
+
+puts "environment file"
---
+++ CLI project/Covid_19_cli/lib/Covid_19_cli/version.rb
@@ -0,0 +1,3 @@
+module Covid19Cli
+  VERSION = "0.1.0"
+end
