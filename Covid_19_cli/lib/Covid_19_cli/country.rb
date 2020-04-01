7cd85a915bd4daa7f38a29df8c1fed48698de9fb
Aniss <59289660+anisssoudki@users.noreply.github.com>
2020-04-01 12:39:03 -0700

    Update country.rb

--- Covid_19_cli/lib/Covid_19_cli/country.rb
+++ Covid_19_cli/lib/Covid_19_cli/country.rb
@@ -1,31 +1,31 @@
 class Country 
 	attr_accessor :country, :city, :province, :lastUpdate, :confirmed, :deaths, :recovered, :keyID
-@@all = []
+	@@all = []
 	def initialize(hash)
-		hash.each do |k, v|
-			self.send("#{k}=", v) if self.respond_to?("#{k}=")
-		end
-		save
+	  hash.each do |k, v|
+	  self.send("#{k}=", v) if self.respond_to?("#{k}=")
+	  end
+	  save
 	end
 	def save
-		@@all << self
+	  @@all << self
 	end
 	def self.all 
-		 @@all
+	  @@all
 	end
 	def self.find_by_country(name)
-		self.all.select do |country|  
-			 country.country.capitalize == name
-		end
-		end
-	def self.find_by_city(name)
-		self.all.select do |country|
-		 country.city.capitalize == name
+	  self.all.select do |country|  
+	  country.country.capitalize == name
+	  end
 	end
+	def self.find_by_city(name)
+	  self.all.select do |country|
+	  country.city.capitalize == name
+	  end
 	end
 	def self.find_by_state(name)
-		self.all.select do |country|
-		 country.province.capitalize == name
-	end
+	  self.all.select do |country|
+	  country.province.capitalize == name
+	  end
 	end
 end
