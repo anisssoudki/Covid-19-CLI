39c4d4c8cf5277372dec3b3827b8402a2c9f9c6e
Aniss <59289660+anisssoudki@users.noreply.github.com>
2020-04-01 12:01:49 -0700

    Update country.rb

--- Covid_19_cli/lib/Covid_19_cli/country.rb
+++ Covid_19_cli/lib/Covid_19_cli/country.rb
@@ -1,15 +1,12 @@
 class Country 
-	attr_accessor :country, :city, :province, :lastUpdated, :confirmed, :death, :recovered
+	attr_accessor :country, :city, :province, :lastUpdate, :confirmed, :deaths, :recovered, :keyID
 @@all = []
-
 	def initialize(hash)
 		hash.each do |k, v|
 			self.send("#{k}=", v) if self.respond_to?("#{k}=")
-			
 		end
 		save
 	end
-
 	def save
 		@@all << self
 	end
